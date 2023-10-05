b63ef296e50503ad8c65e41b908081a2bff8c26d
Theodor Waalberg <twaal@bu.edu>
2023-10-05 16:22:44 +0200

    calibration

--- calibrate.py
+++ calibrate.py
@@ -1,26 +1,31 @@
 import serial
 import sys
+import time
 import csv
 # Set the serial port name and baud rate
-ser = serial.Serial('COM5', 9600)  # Replace 'COM3' with your Arduino's serial port
+ser = serial.Serial('COM5', 9600) 
 
 try:
     while True:
+        ser.reset_input_buffer()
+        time.sleep(1)
         user_input = input("Type the ground truth distance (cm) to get a sensor value: ")
         try:
             ground_truth = float(user_input)
         except:
             print("Please enter a distance in cm.")
-            break
+            pass
 
         if user_input != '':
-            sensor_value = ser.readline().decode().strip()
-            with open('sensor_data.csv', mode='a', newline='') as file:
-                writer = csv.writer(file)
-                
-                # Write the data to the CSV file
-                writer.writerow([ground_truth, sensor_value])
-            print(f"Distance: {ground_truth} Sensor Reading: {sensor_value}")
+            for i in range(30):
+                sensor_value = ser.readline().decode().strip()
+                with open('sensor_data.csv', mode='a', newline='') as file:
+                    writer = csv.writer(file)
+                    
+                    # Write the data to the CSV file
+                    writer.writerow([ground_truth, sensor_value])
+                print(f"Distance: {ground_truth} Sensor Reading: {sensor_value}")
+        
 
 except KeyboardInterrupt:
     pass
--- lab_1/lab_1.ino
+++ lab_1/lab_1.ino
@@ -4,7 +4,7 @@
 // 4) Then go to board manager and install adafruit SAMD
 // 5) Switch board to Adafruit Metro M4
 
-const int sensorPin = A3; // Analog pin for IR sensor
+const int sensorPin = A1; // Analog pin for IR sensor
 int sensorValue;
 float distance;
 
@@ -13,16 +13,27 @@ void setup() {
 }
 
 float getDistance(int Vout) {
+  // Setup transfer function 
+  float k = 13.0; // Multiplier value for transfer function
+  float n = 1.0; // Negative exponential decay rate
+  
   // Convert the analog reading to voltage (in volts)
   float voltage = Vout * (5.0 / 1023.0); // 5V Arduino
 
   // Calculate distance using the formula from the datasheet
-  float distance = 13 * pow(voltage, -1.0);
+  float distance = 13 * pow(Vout, -1.0);
 
   return distance;
 }
 
+void serialFlush(){
+  while(Serial.available() > 0) {
+    char t = Serial.read();
+  }
+}
+
 void loop() {
+  serialFlush();
   sensorValue = analogRead(sensorPin);
   distance = getDistance(sensorValue);
 //  Serial.print("S:"); // Start of distance data marker
